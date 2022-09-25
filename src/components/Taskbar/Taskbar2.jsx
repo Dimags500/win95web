d93af38c0f72903745dd3efbc089d9bb5e40e0c4
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-09-25 21:13:34 +0300

    multi tasks test

--- src/components/Taskbar/Taskbar2.jsx
+++ src/components/Taskbar/Taskbar2.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { TaskBar, Modal, List } from "@react95/core";
+import { TaskBar, Modal, List, Frame } from "@react95/core";
 import { ReaderClosed, WindowsExplorer, Inetcfg2300 } from "@react95/icons";
 import Styles from "../Desktop/styles.module.css";
 import Browser from "../browser/Browser";
@@ -13,12 +13,109 @@ const Taskbar2 = () => {
   const [first, toggleFirst] = React.useState(false);
   const [second, toggleSecond] = React.useState(false);
   const [third, toggleThird] = React.useState(false);
+  const [render, setRender] = React.useState(false);
 
   const closeFirst = () => toggleFirst(false);
   const closeSecond = () => toggleSecond(false);
   const closeThird = () => toggleThird(false);
 
+  const ref = React.useRef([]);
+
+  React.useEffect(() => {
+    if (ref.current.length <= 0) {
+      ref.current.push(
+        <Modal
+          defaultPosition={{ x: 100, y: 50 }}
+          width="300"
+          height="200"
+          icon={<Inetcfg2300 variant="32x32_4" />}
+          title=" Explorer"
+          closeModal={closeThird}
+        >
+          <Frame
+            bg="white"
+            boxShadow="in"
+            height="100%"
+            width="100%"
+            padding="0px 5px"
+          >
+            <p>
+              The active modal will be based on the order they render, most
+              recently rendered will be the active component. On click of a
+              non-active modal will fire an action to set that modal as the
+              active one.
+            </p>
+          </Frame>
+        </Modal>
+      );
+    }
+    console.log(ref.current);
+  });
+
+  const modalArray = [
+    <Modal
+      defaultPosition={{ x: 50, y: 50 }}
+      width="300"
+      height="200"
+      icon={<Inetcfg2300 variant="32x32_4" />}
+      title=" Explorer"
+      closeModal={closeThird}
+    />,
+  ];
+
+  const addModal = () => {
+    console.log("-----");
+
+    ref.current.push(
+      <Modal
+        defaultPosition={{ x: 100, y: 50 }}
+        width="300"
+        height="200"
+        icon={<Inetcfg2300 variant="32x32_4" />}
+        title=" Explorer"
+        closeModal={closeThird}
+      >
+        <Frame
+          bg="white"
+          boxShadow="in"
+          height="100%"
+          width="100%"
+          padding="0px 5px"
+        >
+          <p>
+            The active modal will be based on the order they render, most
+            recently rendered will be the active component. On click of a
+            non-active modal will fire an action to set that modal as the active
+            one.
+          </p>
+        </Frame>
+      </Modal>
+    );
+    setRender(false);
+
+    modelGen();
+    setRender(true);
+
+    renderTaskBar();
+  };
+
+  const modelGen = () => {
+    const modals = (
+      <>
+        {ref.current.map((elem, index) => {
+          elem.props.defaultPosition.y += index * 20;
+          console.log(ref.current.length, "leng");
+          return <div key={index}>{elem}</div>;
+        })}
+      </>
+    );
+
+    return modals;
+  };
+
   const renderTaskBar = () => {
+    console.log(ref.current);
+
     return (
       <>
         {first && (
@@ -42,16 +139,7 @@ const Taskbar2 = () => {
           />
         )}
 
-        {third && (
-          <Modal
-            defaultPosition={{ x: 50, y: 50 }}
-            width="300"
-            height="200"
-            icon={<Inetcfg2300 variant="32x32_4" />}
-            title="React Internet Explorer"
-            closeModal={closeThird}
-          />
-        )}
+        <>{render && modelGen()}</>
 
         <TaskBar
           list={
@@ -73,7 +161,7 @@ const Taskbar2 = () => {
               <List.Item
                 icon={<Inetcfg2300 variant="32x32_4" />}
                 onClick={() => {
-                  toggleThird(true);
+                  addModal();
                 }}
               >
                 React Internet Explorer
