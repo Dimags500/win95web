a0813c12033c8cf6001122f06222b9fc252021f9
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-09-27 13:16:19 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from "./logo.svg";
-// import "./App.css";
 import React from "react";
-import Desktop from "./components/Desktop/Desktop";
-
 import { ThemeProvider } from "@react95/core";
 import "@react95/icons/icons.css";
+import "./App.css";
 import Facade from "./Facade";
 
 const App = () => (
